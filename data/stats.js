6d7a11eb400d02bf614137533c879a18cbf39ade
oldirtychi <ejsapayanp@gmail.com>
2024-09-16 01:56:24 -0500

    stats measured

---
+++ data/stats.js
@@ -0,0 +1,19 @@
+const stats = [
+    {
+        id: 1,
+        hp: "650whp",
+        torque: "500lbft"
+    },
+    {
+        id: 2,
+        hp: "600whp",
+        torque: "450lbft",
+    },
+    {
+        id: 3,
+        hp: "800whp",
+        torque: "750lbft",
+    },
+];
+
+module.exports = stats;
