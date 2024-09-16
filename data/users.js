2f3eaa34a86ceeccdd1f1a8d2371a307c77cffa2
oldirtychi <ejsapayanp@gmail.com>
2024-09-16 01:20:12 -0500

    users

---
+++ data/users.js
@@ -0,0 +1,22 @@
+const users = [
+    {
+        id: 1,
+        name: "Tran",
+        username: "jonnytran69",
+        email: "jonnytran69@email.com",
+    },
+    {
+        id: 2,
+        name: "Hector",
+        username: "hectorspoon",
+        email: "hect66@email.com",
+    },
+    {
+        id: 3,
+        name: "Parker",
+        username: "thetejmahal",
+        email: "tejparker@email.com",
+    },
+];
+
+module.exports = users
--- index.js
+++ index.js
@@ -6,3 +6,23 @@ app.listen(port, () => {
     console.log(`Server is listening on port: ${port}`);
 });
 
+app.get("/", (req, res) => {
+    res.send("Hello world!");
+    // console.log('GET Request');
+});
+
+app.route("/vehicles")
+    .get((req, res) => {
+        res.send("Load a random vehicle");
+    })
+    .post((req ,res) => {
+        res.send("Add a vehicle");
+    })
+    .put((req, res) => {
+        res.send("update a vehicles infomation");
+    });
+
+//error handler    
+app.use((err, req, res, next) => {
+    res.status(400).send('Something Broke!');
+});
